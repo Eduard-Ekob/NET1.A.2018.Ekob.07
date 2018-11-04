4373300b2dba1e5de951e31c8b5b5dc459146e57
ED <852179@mail.ru>
2018-11-04 19:43:02 +0300

    Add project files.

---
+++ JaggedArraySort/JagArrSort.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace JaggedArraySort
+{
+    public static class JagArrSort
+    {
+        /// <summary>
+        /// The BubbleSortSumElemInLine sort jagged array by sum elements in line of array
+        /// </summary>
+        /// <param name="jagArr">jagArr is incomming array for sort</param>
+        /// <exception cref="ArgumentNullException">If incomming array is null</exception>
+        public static void BubbleSortSumElemInLine(int[][] jagArr)
+        {
+            if (jagArr == null)
+            {
+                throw new ArgumentNullException(nameof(jagArr));
+            }
+
+            for (int i = 0; i < jagArr.Length; i++)
+            {
+                for (int j = i + 1; j < jagArr.Length; j++)
+                {
+                    if (jagArr[j] == null )
+                    {
+                        //Swap(ref jagArr[i], ref jagArr[j]);
+                        continue;
+                    }
+
+                    if (SumArrLine(ref jagArr[i]) < SumArrLine(ref jagArr[j]))
+                    {
+                        Swap(ref jagArr[i], ref jagArr[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The SortMaxElemInLine sort jagged array with maximum element in line of array
+        /// </summary>
+        /// <param name="jagArr">jagArr is incomming array for sort</param>
+        /// <exception cref="ArgumentNullException">If incomming array is null</exception>
+        public static void SortMaxElemInLine(int[][] jagArr)
+        {
+            if (jagArr == null)
+            {
+                throw new ArgumentNullException(nameof(jagArr));
+            }
+
+            for (int i = 0; i < jagArr.Length; i++)
+            {
+                for (int j = i + 1; j < jagArr.Length; j++)
+                {
+                    if (jagArr[i] == null)
+                    {
+                        Swap(ref jagArr[i], ref jagArr[j]);
+                        continue;
+                    }
+
+                    if (jagArr[j] == null)
+                    {
+                       
+                        continue;
+                    }
+
+                    if (MaxArrLine(ref jagArr[i]) < MaxArrLine(ref jagArr[j]))
+                    {
+                        Swap(ref jagArr[i], ref jagArr[j]);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The SortMinElemInLine sort jagged array with minimum element in line of array
+        /// </summary>
+        /// <param name="jagArr">jagArr is incomming array for sort</param>
+        /// <exception cref="ArgumentNullException">If incomming array is null</exception>
+        public static void SortMinElemInLine(int[][] jagArr)
+        {
+            if (jagArr == null)
+            {
+                throw new ArgumentNullException(nameof(jagArr));
+            }
+
+            for (int i = 0; i < jagArr.Length; i++)
+            {
+                for (int j = i + 1; j < jagArr.Length; j++)
+                {
+                    if (jagArr[i] == null)
+                    {
+                        continue;
+                    }
+
+                    if (jagArr[j] == null)
+                    {
+                        Swap(ref jagArr[i], ref jagArr[j]);
+                        continue;
+                    }
+
+                    if (MinArrLine(ref jagArr[i]) > MinArrLine(ref jagArr[j]))
+                    {
+                        Swap(ref jagArr[i], ref jagArr[j]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref int[] arrF, ref int[] arrS)
+        {
+            var tempArr = arrF;
+            arrF = arrS;
+            arrS = tempArr;
+        }
+
+        private static int SumArrLine(ref int[] arr)
+        {
+            int sum = 0;
+            if (arr != null)
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+
+            return sum;
+        }
+
+        private static int MinArrLine(ref int[] arr)
+        {
+            int min = arr[0];
+            var minInd = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {                
+                if (min > arr[i])
+                {
+                    min = arr[i];
+                    minInd = i;
+                }
+            }
+
+            return arr[minInd];
+        }
+
+        private static int MaxArrLine(ref int[] arr)
+        {
+            int max = arr[0];
+            var maxInd = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                    maxInd = i;
+                }
+            }
+
+            return arr[maxInd];
+        }
+    }
+
+}
---
+++ JaggedArraySortTests/JaggArrSortTest.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JaggedArraySort;
+
+namespace JaggedArraySortTests
+{
+    [TestFixture]
+    public class JaggArrSortTest
+    {
+        [Test]
+        public void BubbleSortSumElemInLineTest_ReturnSortingArray()
+        {
+            // Assert
+            int[][] InputJagArr = new int[][]
+            {
+                new int[] {1, 3, 5, 7, 9},
+                null,
+                new int[] {0, 2, 4, 6},
+                new int[] {11, 22},
+                null,
+                new int[] {1, 48},
+                new int[] {1, 1, 1, 21}
+            };
+
+            int[][] ExpectedJagArr = new int[][]
+            {
+                new int[] {1, 48},
+                new int[] {11, 22},
+                new int[] {1, 3, 5, 7, 9},
+                new int[] {1, 1, 1, 21},
+                new int[] {0, 2, 4, 6},
+                null,
+                null
+            };
+
+            // Act
+            JagArrSort.BubbleSortSumElemInLine(InputJagArr);
+
+            // Assert
+            CollectionAssert.AreEqual(ExpectedJagArr, InputJagArr);
+        }
+
+        [Test]
+        public void SortMaxElemInLineTest_ReturnSortingArray()
+        {
+            // Assert
+            int[][] inputJagArr = new int[][]
+            {
+                new int[] {1, 3, 5, 7, 9},
+                new int[] {0, 2, 4, 6},
+                null,
+                new int[] {11, 22},
+                null,
+                new int[] {1, 48},
+                new int[] {1, 1, 1, 9952}
+            };
+
+            int[][] expectedJagArr = new int[][]
+            {
+                new int[] {1, 1, 1, 9952},
+                new int[] {1, 48},
+                new int[] {11, 22},
+                new int[] {1, 3, 5, 7, 9},
+                new int[] {0, 2, 4, 6},
+                null,
+                null
+            };
+
+            // Act
+            JagArrSort.SortMaxElemInLine(inputJagArr);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedJagArr, inputJagArr);
+        }
+
+        [Test]
+        public void SortMinElemInLineTest_ReturnSortingArray()
+        {
+            // Assert
+            int[][] inputJagArr = new int[][]
+            {
+                new int[] {1, 3, 5, 7, 9},
+                null,
+                new int[] {0, 2, 4, 6},
+                new int[] {-11, 22},
+                null,
+                new int[] {1, 48},
+                new int[] {1, 1, 1, 21}
+            };
+
+            int[][] expectedJagArr = new int[][]
+            {
+                null,
+                null,
+                new int[] {-11, 22},
+                new int[] {0, 2, 4, 6},
+                new int[] {1, 3, 5, 7, 9},
+                new int[] {1, 48},
+                new int[] {1, 1, 1, 21}
+
+            };
+
+            // Act
+            JagArrSort.SortMinElemInLine(inputJagArr);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedJagArr, inputJagArr);
+        }
+
+        [Test]
+        public void BubbleSortSumElemInLineTest_ThrowsArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => JagArrSort.BubbleSortSumElemInLine(null));
+
+        [Test]
+        public void BubbleSortMaxElemInLineTest_ThrowsArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => JagArrSort.SortMaxElemInLine(null));
+
+        [Test]
+        public void BubbleSortMinElemInLineTest_ThrowsArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => JagArrSort.SortMinElemInLine(null));
+    }
+}
